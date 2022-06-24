16894f2f316f30d368927649d2dd8b8332e5d312
Yusful305 <lawalyusuff02@gmail.com>
2022-06-24 06:35:08 +0100

    javascript calculator

---
+++ calculator.js
@@ -0,0 +1,9 @@
+var num1 = parseInt (prompt('num1: '))
+var opr = prompt("operator: - / * +");
+var num2 =  parseInt (prompt('num2: '))
+
+if (opr === '+') {alert(num1 + num2)}
+	else if (opr === '+'){alert(num1 + num2)}
+	else if (opr === '-'){alert(num1 - num2)}
+	else if (opr === '*'){alert(num1 * num2)}
+	else if (opr === '/'){alert(num1 / num2)}
